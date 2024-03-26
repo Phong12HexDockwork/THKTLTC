fc78e5b053f564d4b50239f0060d3eaa16df77e5
Neraxis_1210 <99865968+Phong12HexDockwork@users.noreply.github.com>
2024-03-26 22:25:24 +0700

    Update main.m

--- Matlab/duma/main.m
+++ Matlab/duma/main.m
@@ -1,20 +1,15 @@
 function main()
   while true
-    % Yêu cầu người dùng nhập lựa chọn
+    
     ch = input('Nhập lựa chọn (1, 2, 3 hoặc 0 để thoát): ');
-
-    % Xử lý lựa chọn
     switch ch
-      case '1'
-        fprintf('Phương pháp chia đôi khoảng cách\n');
+      case 1
         chia_doi_khoang_cach(a, b);
-      case '2'
-        fprintf('Phương pháp lặp\n');
+      case 2
         lap(a, b);
-      case '3'
-        fprintf('Phương pháp Newton\n');
+      case 3
         newton(a, b);
-      case '0'
+      case 0
         fprintf('Thoát chương trình\n');
         break;
       otherwise
