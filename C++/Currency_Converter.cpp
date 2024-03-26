c77d4b69fb71b7c5a4be80629240abb49d16a842
Neraxis_1210 <99865968+Phong12HexDockwork@users.noreply.github.com>
2024-03-26 22:17:33 +0700

    Update Currency_Converter.cpp

--- C++/Currency_Converter.cpp
+++ C++/Currency_Converter.cpp
@@ -10,15 +10,16 @@ string money_detail_transfer[3], type_money [6] = {"vnd", "usd", "won", "baht", 
 void main_money_transfer (int control_menu) {
     //(1.1)Showing main_menu and take the output for each step [1] type money in [2] amount [3] type money out 
     for (control_menu ; control_menu < 3; control_menu++ ) {cout << control_menu + 1 << ". "<< menu_money[control_menu]; cin >> money_detail_transfer [control_menu]; cout << endl;}
+    
     //(1.1**)Comparing which index value in array exist and take [price] of them 
     for (int index = 0; index < 3; index++ ) { for (int jndex = 0; jndex < 6; jndex++ ) {
     if (money_detail_transfer[index] == type_money[jndex]) { index_temp[index] = jndex ;} }}
+    
     //(1.3)Conclusion transfer [string] [amount] to [int] and put it into formula
     sum_money = ( (stoi (money_detail_transfer[1])) * (1/price_currency[index_temp[0]]) ) / (1/price_currency[index_temp[2]]) ;
     cout << sum_money << endl;
 
     return;
-
 }
 
 int after_transfer (int control_menu) { //(2.1)menu exit and take control menu key [continue] or [break]
